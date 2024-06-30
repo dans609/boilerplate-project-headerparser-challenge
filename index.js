0424b1e815e8887f87174ea0a1347e3fec056013
Dandi Anastasa <dandianastasa12@gmail.com>
2024-06-30 17:03:55 +0700

    creating new route to handle the request and send response with inital data

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  const data = {
+    ipadress: null,
+    language: null,
+    software: null,
+  }
+  
+  res.json(data);
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
